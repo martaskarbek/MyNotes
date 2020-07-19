b30e32b6c7051ace4bf2f793962d7d754c6378c1
Marta Skarbek <marta@skarbek.name>
2020-07-19 21:57:00 +0200

    fixed adding values to array

--- script.js
+++ script.js
@@ -20,26 +20,34 @@ function createNewNotepad() {
         const template = document.querySelector('#notepad-template');
         const clone = document.importNode(template.content, true);
         clone.querySelector('section');
-        clone.querySelector('textarea');
-        clone.querySelector('button');
+        clone.querySelector('textarea').classList.add(String(i));
+        clone.querySelector('button').classList.add(String(i));
         return clone;
     };
     const noteElement = createNotepad('Add note');
     document.querySelector('#parentNode').appendChild(noteElement);
 }
 
-
+function firstAdd() {
+     let value = add_area.value;
+    dataFromStorage.push(value);
+}
 
 function print() {
-    let value = add_area.value;
-    dataFromStorage.push(value);
+    let area = document.querySelector('.input');
+    let tempButton = document.querySelector('button');
+    if (area.classList.contains(String(i)) && tempButton.classList.contains(String(i))) {
+        let value2 = area.value;
+        dataFromStorage.push(value2);
+    }
     console.log(dataFromStorage);
-    localStorage.setItem('note', JSON.stringify({i: value}));
-    console.log('dupa');
-    value = "";
-
+    i++;
     const toRemove = document.querySelector('section');
     toRemove.remove();
+}
+
+function addToLocalStorage() {
+    /* localStorage.setItem('note', JSON.stringify({i: value}));*/
 }
 
 main();
