4825295da47c78e4952feaef00579a60d55f8423
Marta Skarbek <marta@skarbek.name>
2020-07-19 16:15:40 +0200

    added printing notes in second area

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 const add_area = document.querySelector('.input');
 const submit_button = document.querySelector('.submit');
-const display_notes = document.getElementsByClassName('notes_show');
+const display_notes = document.querySelector('.notes_show');
 
 function main() {
     console.log(localStorage.getItem("note"));
     submit_button.addEventListener('click', print);
-
+    let x = localStorage.getItem("note");
+    display_notes.innerHTML = x;
 }
 
 function print() {
     console.log('dupa');
     let value = add_area.value;
-    /*console.log(value);*/
     localStorage.setItem("note", value);
-    /*localStorage.getItem("note");*/
-
-
 }
 
-/*
-function add_value_textarea(){
-    var moja_wartosc = 'to jest moja wartosc tekstowa';
-    const add_area = document.querySelector('.input');
-    return add_area.value = moja_wartosc;
-}*/
-window.onload=function(){  add_value_textarea();}
-print();
 main();
