06175f176a6b3b04a1c02099fb8a9a4b6889c870
Marta Skarbek <marta@skarbek.name>
2020-07-19 18:13:57 +0200

    fixed css problem and added template for div

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const add_area = document.querySelector('.input');
 const submit_button = document.querySelector('.submit');
 const display_notes = document.querySelector('.notes_show');
+let i = 0;
 
 function main() {
     console.log(localStorage.getItem("note"));
     submit_button.addEventListener('click', print);
-    let x = localStorage.getItem("note");
-    display_notes.innerHTML = x;
-}
 
+    let jsonArray = localStorage.getItem("note");
+    let notes = JSON.parse(jsonArray);
+    console.log(notes);
+    let p = display_notes.insertAdjacentElement('.notes_show', 'p');
+    p.innerHTML(notes.note);
+        /*  display_notes.innerHTML = x;*/
+
+}
 function print() {
-    console.log('dupa');
     let value = add_area.value;
-    localStorage.setItem("note", value);
+    localStorage.setItem('note', JSON.stringify({i: value}));
+    console.log('dupa');
+
 }
 
 main();
