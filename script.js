8f66004f7fa0f2c270a1592d4290cae5cbf69eb5
Marta Skarbek <marta@skarbek.name>
2020-07-19 18:53:06 +0200

    added creating new text board

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const add_area = document.querySelector('.input');
 const submit_button = document.querySelector('.submit');
 const display_notes = document.querySelector('.notes_show');
+const create_new_note = document.querySelector('.create');
 let i = 0;
 
 function main() {
     console.log(localStorage.getItem("note"));
     submit_button.addEventListener('click', print);
-
+    create_new_note.addEventListener('click', function() {
+        const createNotepad = function(button_name){
+            let toRemove = document.querySelector('section');
+            toRemove.remove();
+            const template = document.querySelector('#notepad-template');
+            const clone = document.importNode(template.content, true);
+            clone.querySelector('section');
+            clone.querySelector('textarea');
+            clone.querySelector('button').textContent = button_name;
+            return clone;
+        };
+        const rowElement = createNotepad('Add note');
+        document.querySelector('#parentNode').appendChild(rowElement);
+    })
     let jsonArray = localStorage.getItem("note");
     let notes = JSON.parse(jsonArray);
     console.log(notes);
-    let p = display_notes.insertAdjacentElement('.notes_show', 'p');
-    p.innerHTML(notes.note);
-        /*  display_notes.innerHTML = x;*/
 
 }
 function print() {
+    let data = [];
     let value = add_area.value;
     localStorage.setItem('note', JSON.stringify({i: value}));
     console.log('dupa');
